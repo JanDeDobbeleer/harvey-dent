7c643b14d0964b5e636467525e8a7d6a9e82bfaf
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2017-10-10 14:04:16 +0200

    Rename fin to logo

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -3,8 +3,8 @@ import time
 from scripts.tickets import get_jira_info
 
 # print a pretty logo, because fork yeah
-with open('scheduler.txt', 'r') as fin:
-    print(fin.read())
+with open('scheduler.txt', 'r') as logo:
+    print(logo.read())
 
 # schedule jobs to run
 schedule.every(5).minutes.do(get_jira_info)
