8f64ae364cb9e25f8413b6007b1a068d8277f146
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2017-11-08 10:14:25 +0100

    Added settings file

--- harvey/harvey.py
+++ harvey/harvey.py
@@ -1,40 +1,21 @@
-try:
-    import time
-    import schedule
-    import scripts
-except Exception as e:
-    print("Error during import: "+ e)
+import time
+import schedule
+import yaml
 
 # print a pretty logo, because fork yeah
 with open('harvey.txt', 'r') as logo:
     print(logo.read())
 
-JIRA_QUERIES = {
-    "uwa_open_bugs": "UWA: Open bugs",
-    "uwa_backlog_depth": "UWA: Backlog",
-    "uwa_sprint_completed": "UWA: Sprint Completed",
-    "uwa_sprint_todo": "UWA: Sprint Todo"
-}
-
-APPS = {
-    "app_store_vikingapp_android": "com.vikingco.vikingapp",
-    "app_store_stievie_android": "be.stievie"
-}
-
-APPS2 = {
-    "app_store_vikingapp_ios": "viking-app/id893916390",
-    "app_store_stievie_ios": "stievie/id1049241602"
-}
-
 try:
-    j = scripts.JiraTickets(JIRA_QUERIES)
-    schedule.every(5).minutes.do(j.run)
-    p = scripts.PlayStore(APPS)
-    schedule.every(1440).minutes.do(p.run)
-    a = scripts.AppStore(APPS2)
-    schedule.every(1440).minutes.do(a.run)
+    with open('tasks.yaml', 'r') as tasks:
+        _settings = yaml.load_all(tasks)
+        module = __import__("scripts")
+        for task in _settings:
+            class_ = getattr(module, task['script'])
+            instance = class_(task['settings'])
+            schedule.every(task['interval']).minutes.do(instance.run)
 except Exception as e:
-    print(e)
+        print(e)
 
 while True:
     schedule.run_pending()
