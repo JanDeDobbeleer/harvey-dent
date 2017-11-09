934ee92a05363aba5024c9832335476b185b5dcd
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2017-11-09 10:00:55 +0100

    Init graphite on each insert

--- harvey/scripts/scriptbase.py
+++ harvey/scripts/scriptbase.py
@@ -7,12 +7,6 @@ class ScriptBase(object):
     """The base script implementation containing all you need."""
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self):
-        graphitesend.init(graphite_server=os.environ.get('GRAPHITE_SERVER'),
-                          graphite_port=int(os.environ.get('GRAPHITE_PORT')),
-                          prefix=os.environ.get('GRAPHITE_PREFIX'),
-                          system_name=os.environ.get('GRAPHITE_NAME'))
-
     @abc.abstractmethod
     def run(self):
         """Retrieve data from the settings source and run the data retrieval"""
@@ -24,6 +18,12 @@ class ScriptBase(object):
     def _write_point(self, measurement, value, timestamp):
         """Write the values to the Carbon data store"""
         try:
+            graphitesend.init(graphite_server=os.environ.get('GRAPHITE_SERVER'),
+                              graphite_port=int(os.environ.get('GRAPHITE_PORT')),
+                              prefix=os.environ.get('GRAPHITE_PREFIX'),
+                              system_name=os.environ.get('GRAPHITE_NAME'))
             graphitesend.send(measurement, value, timestamp)
+            graphitesend.reset()
         except ConnectionError as error:
             print("Can't connect to the database...")
+
